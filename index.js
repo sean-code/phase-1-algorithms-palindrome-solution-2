250180016f90c45bf7d8b17678ba9343111d5073
sean-code <john.nganga@student.moringaschool.com>
2022-08-26 14:33:31 +0300

    Lab Done

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    //compare firstletter with last letter
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
 
 /*
   Add written explanation of your solution here
